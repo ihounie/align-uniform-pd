6b67581a0d6cd412e678179498e0b06828a80555
ihounie <ihounie@seas.upenn.edu>
2022-12-08 17:06:04 -0500

    seed everything

--- examples/stl10/util.py
+++ examples/stl10/util.py
@@ -1,4 +1,16 @@
 import torch
+import os
+import numpy as np
+
+def seed_everything(seed: int):    
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)  
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
 
 
 class AverageMeter(object):
